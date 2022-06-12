1ad98f1a2769805127caba3849b9073e6f4c5bfc
Denis Loza <denisloza.git@gmail.com>
2022-06-12 14:02:46 +0300

    Step 2. fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
  
 const program = new Command();
 program
+  .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
